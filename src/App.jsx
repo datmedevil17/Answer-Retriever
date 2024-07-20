7517e749002c4378a1be0089eb14b152dba54176
datmedevil17 <rakshitshukla17@gmail.com>
2024-07-20 13:37:08 +0530

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <input type="file" />
+      <button>Question</button>
+      <br />
+      <p>{}</p>
+    </>
+  )
+}
+
+export default App
